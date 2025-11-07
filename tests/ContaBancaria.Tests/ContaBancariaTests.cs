85ab6943c0a67c3bfa34b6468a73f79a2b2ecbcd
Matheus Gomes <matt-xp2@live.com>
2025-11-07 19:35:19 -0300

    feat: criado um teste para a conta nao comecar com saldo diferente de zero

--- tests/ContaBancaria.Tests/ContaBancariaTests.cs
+++ tests/ContaBancaria.Tests/ContaBancariaTests.cs
@@ -16,4 +16,17 @@ public class ContaBancariaTests
         // Assert
         Assert.Equal(0, saldoInicial);
     }
+    
+    [Fact]
+    public void NovaContaNaoDeveComecarComSaldoInicialDiferenteDeZero()
+    {
+        // Arrange
+        var conta = new tdd_conta_bancaria.Models.ContaBancaria();
+
+        // Act
+        var saldoInicial = conta.Saldo;
+
+        // Assert
+        Assert.NotEqual(100, saldoInicial);
+    }
 }
